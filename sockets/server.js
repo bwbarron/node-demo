e1ce2730aa386a7c18f60d8b1bb1d0e0f3884f52
bwbarron <brandon.barron@live.com>
2015-12-03 17:15:56 -0800

    sockets n stuff

--- sockets/server.js
+++ sockets/server.js
@@ -1,5 +1,37 @@
-/*
-    server.js
-    main server script for the socket.io chat demo
-*/
+var net = require("net");
+var server = net.createServer();
+var clients = [];
 
+server.on("connection", function (socket) {
+    var name;
+
+    function broadcast(name, message) {
+        clients.forEach(function (client) {
+            if (client !== socket) {
+                client.write("[" + name + "] " + message);
+            }
+        });
+    }
+
+    clients.push(socket);
+    socket.write("Hello!\n");
+    socket.on("data", function (data) {
+        if (!name) {
+            name = data.toString().trim();
+            socket.write("Hello " + name + "!\n");
+        } else {
+            var message = data.toString();
+            if (message.trim() == "exit") {
+                socket.end();
+            } else {
+                broadcast(name, message);
+            }
+        }
+    });
+});
+
+server.on("listening", function () {
+    console.log("server listening on port 3000");
+});
+
+server.listen(3000);
